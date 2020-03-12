257ad0ca0fb5b336dd674ce26664080d03a08cdc
Andrew30121987 <andre30121987@gmail.com>
2020-03-12 16:03:24 +0200

    Creat JS

---
+++ main.js
@@ -0,0 +1,53 @@
+// alert('Hello World');
+// alert('Hello JS');
+
+/**
+ *
+ * Переменые
+ */
+let myMessage;
+myMessage = "Сообщение";
+
+let myNumber = 10.484;
+let myString = "Hello Vasya";
+let myBoolean = true;
+let myNull = null;
+let myUndefined = undefined;
+let myObject = {
+    name: "Vasya",
+    age: 25
+};
+
+/**
+ *
+ * Числа
+ *
+ */
+// console.log(40 + myNumber);
+// console.log(40 - myNumber);
+// console.log(40 * myNumber);
+// console.log(40 / myNumber);
+// myNumber++;
+// let result = 40 + 10;
+// // alert(result);
+// console.log(result + myNumber);
+// console.log(Math.round(5.45));
+// let newNumber = 2.437;
+// console.log(newNumber.toFixed(1));
+
+//Строки
+//
+// console.log("40" + myNumber);
+// console.log(myString.toLowerCase());
+// console.log(myString.toUpperCase());
+
+
+//Mассивы
+
+let names = ["Vasya", "Petya", "Jenya"];
+console.log(names[1]);
+
+
+
+
+
